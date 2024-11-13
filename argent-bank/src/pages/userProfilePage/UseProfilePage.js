27abb01377e0ccf29b8b5bc23d2a29b797e8226a
Alexandre <alexmiquel888@gmail.com>
2024-11-13 12:28:51 +0100

    Gestion du formulaire

--- argent-bank/src/pages/userProfilePage/UseProfilePage.js
+++ argent-bank/src/pages/userProfilePage/UseProfilePage.js
@@ -1,24 +1,44 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   disconnect,
   disconnectThunk,
   selectUser,
+  selectToken,
+  selectUserName,
 } from '../loginPage/loginSlice';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 const UserProfilePage = () => {
   const user = useSelector(selectUser);
+  const token = useSelector(selectToken);
+  const getUserName = useSelector(selectUserName);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { disconnectStatus } = useSelector((state) => state.login);
+
+  const [isVisible, setIsVisible] = useState(false);
+  const [userName, setUserName] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    if (!userName) {
+      setError('Le champs est vide');
+    } else {
+      setError('');
+    }
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(setUserName({ token, userName }));
+  };
 
   useEffect(() => {
     if (disconnectStatus === 'succeeded') {
       navigate('/login');
     }
-    console.log('disconnectStatus :', disconnectStatus);
   }, [disconnectStatus, navigate]);
 
   return (
@@ -35,7 +55,7 @@ const UserProfilePage = () => {
         <div className="flex-row">
           <div className="main-nav-item">
             <i className="fa fa-user-circle"></i>
-            <p>{user.body ? user.body.firstName : ''}</p>
+            <p>{getUserName ? getUserName : ''}</p>
           </div>
           <button
             className="main-nav-item nav-button-userProfile"
@@ -54,15 +74,73 @@ const UserProfilePage = () => {
             Welcome back
             <br />
             <p>
-              {' '}
               {/* Ici l'opération ternaire nous permet d'attendre la récupèration de 
                     user.body et ainsi éviter les erreurs de rendu. */}
-              {user.body
-                ? `${user?.body.firstName} ${user?.body.lastName}`
-                : ''}
+              {user.body ? `${user.body.firstName} ${user.body.lastName}` : ''}
             </p>
           </h1>
-          <button className="edit-button">Edit Name</button>
+          <button
+            className="edit-button"
+            onClick={() => {
+              setIsVisible(true);
+            }}
+          >
+            Edit Name
+          </button>
+          <div
+            className={isVisible ? 'form-wrapper-flex' : 'form-wrapper-none'}
+          >
+            <form className="user-form">
+              <legend className="user-form-title">Edit user info</legend>
+              <div className="user-form-inputs">
+                <div className="user-form-input">
+                  <label htmlFor="userName">User name:</label>
+                  <input
+                    type="text"
+                    id="userName"
+                    name="userName"
+                    onChange={(e) => setUserName(e.target.value)}
+                  ></input>
+                  <p>{error}</p>
+                </div>
+                <div className="user-form-input">
+                  <label htmlFor="firstName">First name:</label>
+                  <input
+                    className="input-readOnly"
+                    type="text"
+                    id="firstName"
+                    name="firstName"
+                    placeholder={user.body ? user.body.firstName : ''}
+                    readOnly
+                  ></input>
+                </div>
+                <div className="user-form-input">
+                  <label htmlFor="lastName">Last name:</label>
+                  <input
+                    className="input-readOnly"
+                    type="text"
+                    id="lastName"
+                    name="lastName"
+                    placeholder={user.body ? user.body.lastName : ''}
+                    readOnly
+                  ></input>
+                </div>
+              </div>
+              <div className="user-form-buttons">
+                <button type="submit" onSubmit={() => handleSubmit()}>
+                  Save
+                </button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setIsVisible(false);
+                  }}
+                >
+                  Cancel
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
         <h2 className="sr-only">Accounts</h2>
         <section className="account">
